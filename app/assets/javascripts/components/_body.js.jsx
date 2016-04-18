ef75812db8db29c9e465734baa6ecddb33f2096f
Steven Olson <steveoscaro@gmail.com>
2016-04-18 14:28:22 -0600

    add functionality for adding new links

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -10,10 +10,17 @@ var Body = React.createClass({
               (response) => { this.setState({ links: response }) });
   },
 
+  handleSubmit(link) {
+    var newState = this.state.links.concat(link);
+    this.setState({links: newState})
+  },
+  
   render() {
     return (
       <div>
-
+        <div>
+          <NewLink user_id={this.props.user_id} handleSubmit={this.handleSubmit} />
+        </div>
         <div>
           <AllLinks links={this.state.links} />
         </div>
