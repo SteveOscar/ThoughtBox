add403284561fe5d736185ba1eb72cfcdbcfa8e2
Steven Olson <steveoscaro@gmail.com>
2016-04-18 10:54:37 -0600

    add tests for link api index and create actions

--- db/migrate/20160418163339_create_links.rb
+++ db/migrate/20160418163339_create_links.rb
@@ -3,7 +3,7 @@ class CreateLinks < ActiveRecord::Migration
     create_table :links do |t|
       t.string :url
       t.string :title
-      t.boolean :read
+      t.boolean :read, default: false
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
---
+++ test/controllers/api/v1/links_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class Api::V1::LinksControllerTest < ActionController::TestCase
+  # def setup
+  #   request.env['HTTP_AUTHORIZATION'] = "Token token=#{ENV["RAMBLE_MAP_ID"]}"
+  # end
+
+  test "#index" do
+    get :index, user_id: User.first.id, format: :json
+
+    links = JSON.parse(response.body)
+    sample_country = links.first
+
+    assert_response :success
+    assert_equal 1, links.count
+  end
+
+  test "#create" do
+    id = User.first.id
+    initial_count = Link.count
+    get :create, link: { title: 'test link', url: 'www.cnn.com', user_id: id }, format: :json
+
+    data = JSON.parse(response.body)
+    final_count = Link.count
+    assert_response :success
+    assert final_count >  initial_count
+  end
+end
